453e761df0820670b66d406a5c03da104689d785
willianiago <145725672+willianiago@users.noreply.github.com>
2024-09-27 17:31:29 -0300

    adicionei algumas colunas no usuário

--- schema.sql
+++ schema.sql
@@ -1,8 +1,12 @@
 CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    cpf int PRIMARY KEY,
     nome VARCHAR(100),
+    usuario VARCHAR(15),
     email VARCHAR(100),
-    senha VARCHAR(100)
+    senha VARCHAR(100),
+    data_nascimento data(12),
+    forma_pagamento VARCHAR (20),
+    endereço VARCHAR (25)
 );
 
 insert into usuarios("Willian iago", "williantiago0304", "18150806WK")
